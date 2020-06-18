1ad324b2afc6132784069476a1683b7d5ca0fd3a
CPU_Code <923992029@qq.com>
2020-06-18 20:01:18 +0800

    .cpp修改

--- Layout/main.cpp
+++ Layout/main.cpp
@@ -1,8 +1,8 @@
 /*
  * @Author: cpu_code
  * @Date: 2020-06-18 11:25:12
- * @LastEditTime: 2020-06-18 11:26:25
- * @FilePath: \QT\Layout\main.c
+ * @LastEditTime: 2020-06-18 20:00:36
+ * @FilePath: \QT\Layout\main.cpp
  * @Gitee: https://gitee.com/cpu_code
  * @CSDN: https://blog.csdn.net/qq_44226094
  */ 
